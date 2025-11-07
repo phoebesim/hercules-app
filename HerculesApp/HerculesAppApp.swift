818f818480a53653cfb612b442ae8dbdfe394f5a
Phoebe <phoebesimhn@gmail.com>
2025-11-07 18:11:13 +0800

    Initial Commit

---
+++ HerculesApp/HerculesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HerculesAppApp.swift
+//  HerculesApp
+//
+//  Created by Phoebe Sim on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HerculesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
