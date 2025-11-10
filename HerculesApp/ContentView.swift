d65a7fdb5287989e83b9f5a6fb2c0f4dfe3a1950
Phoebe <phoebesimhn@gmail.com>
2025-11-10 09:01:17 +0800

    test

--- HerculesApp/ContentView.swift
+++ HerculesApp/ContentView.swift
@@ -4,10 +4,11 @@
 //
 //  Created by Phoebe Sim on 7/11/25.
 //
-//hiiii
+
+
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View   {
     var body: some View {
         VStack {
             Image(systemName: "globe")
