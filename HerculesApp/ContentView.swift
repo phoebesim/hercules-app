d1df82c975b63b34b25da62fed0754e4f7cb5132
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-11-10 12:52:08 +0800

    rtwrtg

--- HerculesApp/ContentView.swift
+++ HerculesApp/ContentView.swift
@@ -5,21 +5,48 @@
 //  Created by Phoebe Sim on 7/11/25.
 //
 
-
 import SwiftUI
-
-struct ContentView: View   {
+struct ContentView: View {
+    @State private var showingPopover = false
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world")
+        
+        Button("Instructions") {
+            showingPopover = true
         }
-        .padding()
+        .popover(isPresented: $showingPopover) {
+            Text("Instructions: Scan different items based on the requirements! Press ? for more information about the game. Complete all 12 labours to win!")
+                .font(.headline)
+                .padding()
+        }
+        
+        ZStack{
+            Image(.hello)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400, height: 700)
+        }
+        
+        VStack{
+            Button("Start Game") {
+                
+                
+                
+                
+                
+                
+                
+                
+                
+            }
+            
+        }
+        
     }
+    
 }
-
 #Preview {
     ContentView()
 }
