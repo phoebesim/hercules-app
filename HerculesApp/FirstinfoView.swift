7d743bbf69b5e88073f501677d73960d3e0f6d9b
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-11-10 11:29:56 +0800

    fewefwd

--- HerculesApp/FirstinfoView.swift
+++ HerculesApp/FirstinfoView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct FirstinfoView: View   {
+    let textToType = "You have incurred Hera’s wrath...Because you exist. You are a result of Zeus’ infidelity. Hera tried to stop your birth. Hera tried to kill you as a baby. Hera turned you into a murderer. Hera made you a slave for your cousin and king Eurystheus. Now, you must perform 10 labours for him. You are Hercules."
+    
+    @State private var animatedText: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world")
+            //Image("Hercules")
+            //  .resizable()
+            //.aspectRatio(contentMode: .fill)
+            //.ignoresSafeArea()
+            //.brightness(-0.9)
+            VStack {
+                Text(animatedText)
+                    .font(.title)
+                    .padding()
+            }
+            .onAppear()
+            
         }
         .padding()
     }
