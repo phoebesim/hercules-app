b01ae1fb9f5dc269556fa36d9c4fc740c89e8678
Phoebe <phoebesimhn@gmail.com>
2025-11-11 19:54:36 +0800

    added emeralds camera picker and path code

---
+++ HerculesApp/Important/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HerculesApp
+//
+//  Created by Phoebe Sim on 7/11/25.
+//
+
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        Text ("hi")
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
