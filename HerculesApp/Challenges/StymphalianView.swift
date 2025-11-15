b52f3ac666f11e8fa2f1b64a352fcd05bbbef681
Phoebe <phoebesimhn@gmail.com>
2025-11-15 09:44:33 +0800

    stymphalian plus camera

---
+++ HerculesApp/Challenges/StymphalianView.swift
@@ -0,0 +1,18 @@
+//
+//  StymphalianView.swift
+//  HerculesApp
+//
+//  Created by Phoebe Sim on 14/11/25.
+//
+
+import SwiftUI
+
+struct StymphalianView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StymphalianView()
+}
