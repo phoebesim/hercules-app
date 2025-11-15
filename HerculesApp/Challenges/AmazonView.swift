b4586e3b4f666515fde021d23c51f8316042a3c2
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-11-15 09:00:35 +0800

    getgrfefgrrgf

---
+++ HerculesApp/Challenges/AmazonView.swift
@@ -0,0 +1,18 @@
+//
+//  AmazonView.swift
+//  HerculesApp
+//
+//  Created by T Krobot on 14/11/25.
+//
+
+import SwiftUI
+
+struct AmazonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AmazonView()
+}
