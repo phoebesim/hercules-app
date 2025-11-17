2619a66451b015af9f8b5524980546e51dd338bf
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-11-17 15:08:47 +0800

    start page

--- HerculesApp/QuestView.swift
+++ HerculesApp/QuestView.swift
@@ -13,24 +13,147 @@ struct QuestView: View {
     @State private var showingPopover1 = false
     
     var body: some View {
-        
+        ZStack {
+            Image("Grass") // The image at the bottom
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .brightness(0.1)
+                
         
         Button("Instructions") {
             showingPopover = true
         }
+            
+        .zIndex(2)
+        .foregroundColor(.black)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .offset(x: -120, y: -370)
         .popover(isPresented: $showingPopover) {
             Text("Instructions: Scan different items based on the requirements! Press ? for more information about the game. Complete all 12 labours to win!")
                 .font(.headline)
                 .padding()
         }
+            VStack {
+                Button{
+                  BeforelioninfoView()
+                }label: {
+                    Image("Lion")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: -150)
+                    
+                }
+                Button{
+                    
+                }label: {
+                    Image("x")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: -150)
+                    
+                }
+                Button{
+                 DeerView()
+                }label: {
+                    Image("Deer")
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .offset(x: -150)
+                    
+                }
+                Button{
+                    
+                }label: {
+                    Image("x")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: -150)
+                    
+                }
+                Button{
+                    
+                }label: {
+                    Image("Stable")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: -150)
+                    
+                }
+                Button{
+                    
+                }label: {
+                    Image("StymphalianBirdFlying")
+                        .resizable()
+                        .frame(width: 100, height: 200)
+                        .offset(x: -150)
+                    
+                }
+            }
+                VStack {
+               
+                Button{
+                    
+                }label: {
+                    Image("x")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: 150)
+                        
+                }
+                Button{
+                    
+                }label: {
+                    Image("x")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: 150)
+                        
+                }
+                Button{
+                    
+                }label: {
+                    Image("Amazon")
+                        .resizable()
+                        .frame(width: 50, height: 100)
+                        .offset(x: 150)
+                        
+                }
+                Button{
+                    
+                }label: {
+                    Image("Geryon")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: 150)
+                        
+                }
+                Button{
+                    
+                }label: {
+                    Image("x")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: 150)
+                        
+                }
+                Button{
+                    
+                }label: {
+                    Image("Cerberus")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: 150)
+                        
+                }
+
+            }
+            
         
         
-        ZStack {
-            Image(.hello) // The image at the bottom
-                .resizable()
-                .scaledToFill()
-                .frame(width: 400, height: 600)
-                .clipped()
+                
             
             
             Button{
