50cb296018b1ea238f5d1d8492cb8c21201fd4b4
Ning Xuan <wee_ning_xuan_2028@mgs.sch.edu.sg>
2025-11-17 14:12:18 +0800

    part of lionview

--- HerculesApp/Challenges/1LionView.swift
+++ HerculesApp/Challenges/1LionView.swift
@@ -10,95 +10,149 @@ import Combine
 
 struct LionView: View {
     @State private var yOffset: CGFloat = 0
-    @State private var imagePosition: CGPoint? = nil
     @State private var success: Bool = false
- 
+    @State private var arrowYPosition: CGFloat = 150
+    @State private var arrowDirection: CGFloat = 1
+    @State private var isShooting = false
+    @State private var shotArrowXPosition: CGFloat = 120
+    @State private var shotArrowOpacity: Double = 0
+    @State private var hasWon: Bool = false
+    
+    let timer = Timer.publish(every: 0.03, on: .main, in: .common).autoconnect()
+    let greenZoneRange: ClosedRange<CGFloat> = 50...150
+    let yellowZoneRange: ClosedRange<CGFloat> = 150...250
+    let redZoneRange: ClosedRange<CGFloat> = 250...350
+    
     
     var body: some View {
-       
-        ZStack{
-            Image("Grass")
-
-            
-            VStack {
-                Text("Shoot the Nemean Lion!")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .bold()
-                ZStack {
+        GeometryReader { geometry in
+            ZStack{
+                Image("Grass")
+                
+                if hasWon {
+                    ZStack {
+                        Spacer()
+                        WinView()
+                        Spacer()
+                    }
+                }
+                VStack {
+                    Spacer()
+                    Text("Shoot the Nemean Lion!")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .bold()
+                    
                     Image("Lion")
                         .resizable()
                         .frame(width: 300, height:300)
                     
-                    if success {
-                        Image(.arrowCutout)
+                    ZStack {
+                        Image("Bow")
                             .resizable()
-                            .frame(width: 120, height: 120)
-                            .offset(x:0, y:100)
-                            .zIndex(1)
-                    }
-                }
-                ZStack {
-                    Image("Bow")
-                        .resizable()
-                        .frame(width: 250, height: 150)
-                        .padding()
-
-                    
-                    if !success {
-                        Image(.arrowCutout)
-                            .resizable()
-                            .frame(width: 120, height: 220)
-                            .zIndex(1)
+                            .frame(width: 250, height: 150)
+                            .padding()
                         
+                        
+                        if !success {
+                            Image("Pencil")
+                                .resizable()
+                                .frame(width: 120, height: 220)
+                                .zIndex(1)
+                            
+                        }
                     }
-                                    }
-                
-    
-                HStack {
-                    Image("wall")
-                        .resizable()
-                        .frame(width: 60, height: 120)
-                        .offset(y: -50)
                     
-                    Image(systemName: "arrowshape.left.fill")
-                        .offset(y: yOffset)
-                        .foregroundStyle(.white)
-                        .onAppear {
-                            withAnimation(.easeInOut(duration: 0.3).repeatForever(autoreverses: true)) {
-                                yOffset = -100
+                    
+                    HStack(alignment: .bottom, spacing: 0){
+                        VStack (spacing:0) {
+                            
+                            
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color.green)
+                                    .frame(width: 60, height: 100)
+                                    .border(Color.green.opacity(0.7), width: 3)
+                                Text("GREEN")
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                            }
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color.yellow)
+                                    .frame(width: 60, height: 100)
+                                    .border(Color.yellow.opacity(0.7), width: 3)
+                                Text("YELLOW")
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                            }
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color.red)
+                                    .frame(width: 60, height: 100)
+                                    .border(Color.red.opacity(0.7), width: 3)
+                                Text("RED")
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
                             }
                         }
-                    
-                    
-                    Button("Shoot!") {
-                    
-                        if yOffset == -100 {
-                        success = true
-                                
-                        } else {
-                            success = false
+                        
+                        
+                        Image(systemName: "arrowshape.left.fill")
+                            .offset(y: yOffset)
+                            .foregroundStyle(.white)
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 0.3).repeatForever(autoreverses: true)) {
+                                    yOffset = -100
+                                }
+                            }
+                        if shotArrowOpacity > 0 {
+                            VStack {
+                                Spacer()
+                                HStack {
+                                    Spacer()
+                                    
+                                    Image("Pencil")
+                                        .resizable()
+                                        .frame(width: 120, height:220)
+                                        .offset(x: shotArrowXPosition - geometry.size.width + 200)
+                                        .opacity(shotArrowOpacity)
+                                    
+                                }
+                            }
                         }
+                        
+                        
+                        Button("Shoot!") {
+                            
+                            if yOffset == -100 {
+                                success = true
+                                
+                            } else {
+                                success = false
+                            }
+                        }
+                        .font(.title2)
+                        .padding(50)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .clipShape(Circle())
+                        .offset(x:0)
+                        
                     }
-                    .font(.title2)
-                    .padding(50)
-                    .background(Color.white)
-                    .foregroundColor(.black)
-                    .clipShape(Circle())
-                    .offset(x:0)
-                    .padding()
+                    
+                    Spacer()
                 }
-            
+                .padding(.trailing, 50)
                 
-            }
-            .padding(.trailing, 50)
-            
-            Spacer()
-            .frame(height: 200)
-            
-            
-            VStack {
-
+                Spacer()
+                    .frame(height: 200)
+                
+                Spacer()
+                
             }
         }
     }
