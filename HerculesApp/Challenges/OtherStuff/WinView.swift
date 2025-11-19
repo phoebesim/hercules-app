88a97c71ff47bc3fc32a3dd1ebd7fef36b2b5628
Phoebe <phoebesimhn@gmail.com>
2025-11-19 20:57:18 +0800

    winview

--- HerculesApp/Challenges/OtherStuff/WinView.swift
+++ HerculesApp/Challenges/OtherStuff/WinView.swift
@@ -10,16 +10,26 @@ import SwiftUI
 struct WinView: View {
     @Binding var nextInfoView: any View
     var body: some View {
-            Text("You won!!!")
-                .font(.system(size: 48, weight: .bold))
-                .foregroundColor(.black)
-                .transition(.scale)
-                .zIndex(1)
-            Color.white.opacity(0.85)
-                .ignoresSafeArea()
-                .transition(.opacity)
-                .zIndex(3)
-            
+        NavigationStack {
+            ZStack {
+                Color.white.opacity(0.85)
+                    .ignoresSafeArea()
+                    .transition(.opacity)
+                VStack {
+                    Text("You won!!!")
+                        .font(.system(size: 48, weight: .bold))
+                        .foregroundColor(.black)
+                        .transition(.scale)
+           /*         NavigationLink {
+                        AnyView(nextInfoView)
+                    } label: {
+                        Text("Continue")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .background*/
+                }
+            }
+        }
         
     }
 }
