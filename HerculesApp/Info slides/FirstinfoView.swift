8c9902582efb8cde11b6c49e06b79951e1bf3275
Emerald2012 <phyoemeraldaung@gmail.com>
2025-11-28 15:22:44 +0800

    changed to lowercase

--- HerculesApp/Info slides/FirstinfoView.swift
+++ HerculesApp/Info slides/FirstinfoView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct FirstinfoView: View   {
-    let textToType = "You have incurred Hera’s wrath...Because you exist. You are a result of Zeus’ infidelity. Hera tried to stop your birth. Hera tried to kill you as a baby. Hera turned you into a murderer. Hera made you a slave for your cousin and king Eurystheus. Now, you must perform 10 labours for him. You are Hercules."
+    let textToType = "You have incurred Hera’s wrath...because you exist. You are a result of Zeus’ infidelity. Hera tried to stop your birth. Hera tried to kill you as a baby. Hera turned you into a murderer. Hera made you a slave for your cousin and king Eurystheus. Now, you must perform 10 labours for him. You are Hercules."
     
     @State private var animatedText: String = ""
 //    @Binding var homePage: Bool
